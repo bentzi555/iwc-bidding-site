c043925971ac9d1a5019d07af87a67e32325af58
bentzi555 <ben5673450@gmail.com>
2025-02-22 11:30:27 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Welcome to IWC Pre-Owned Watches Bidding</h1>
+      <p>This is a demo site for bidding on luxury pre-owned IWC watches.</p>
+    </div>
+  );
+}
